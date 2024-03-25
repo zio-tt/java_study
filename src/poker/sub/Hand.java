8c7828c7f2f6127c51776d3a7d5f829ddb70cc47
zio-tt <zio.tt.dev@gmail.com>
2024-03-25 09:36:41 +0900

    Fix: subクラスの一部をpoker.subパッケージに移動

---
+++ src/poker/sub/Hand.java
@@ -0,0 +1,23 @@
+package poker.sub;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hand {
+  private List<Card> hand;
+
+  // Constructor
+  public Hand(Deck deck) {
+    List<Card> newHand = new ArrayList<Card>();
+
+    for(int i = 0; i < 5; i++) {
+      Card drawCard = deck.draw();
+      newHand.add(drawCard);
+    }
+
+    this.hand = newHand;
+  }
+
+  // Getter
+  public List<Card> getHand() { return hand; }
+}
